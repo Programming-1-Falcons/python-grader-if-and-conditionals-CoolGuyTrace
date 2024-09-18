249716b4be3fd74ba857173b7562448588b88a15
CoolGuyTrace <christra011@alpinesd.org>
2024-09-18 09:51:34 -0600

    Update grader.py

--- grader.py
+++ grader.py
@@ -4,12 +4,12 @@ points = int(input("Enter the amount of points the student got: "))
 score = (points/total_points)*100
 
 if score <= 50:
-    print("The students grade is an:", possible_grades[-1])
+    print("The students grade is a(n):", possible_grades[-1])
 elif score > 50 and score <= 60:
     print("The students grade is a(n):", possible_grades[3])
 elif score > 60 and score <= 75:
-    print("The students grade is an:", possible_grades[2])
+    print("The students grade is a(n):", possible_grades[2])
 elif score > 75 and score <= 88:
-    print("The students grade is an:", possible_grades[1])
+    print("The students grade is a(n):", possible_grades[1])
 else:
-    print("The students grade is an:", possible_grades[0])
+    print("The students grade is a(n):", possible_grades[0])
